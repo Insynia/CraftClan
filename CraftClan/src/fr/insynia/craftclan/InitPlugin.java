62fd549792e0bafd5adce7fbee7e18af64ab0c11
Terry Raimondo <terry.raimondo@gmail.com>
2015-06-08 11:13:21 +0200

    Changing farmzone reset time

--- CraftClan/src/fr/insynia/craftclan/InitPlugin.java
+++ CraftClan/src/fr/insynia/craftclan/InitPlugin.java
@@ -29,7 +29,7 @@ public class InitPlugin {
                                     }
                                 }, 60 * 5 * 20); // 5 minutes
                     }
-                }, 60 * 60 * 20, 60 * 60 * 20); // 1 hour
+                }, 60 * 20, 60 * 60 * 20 * 6); // 6 hours
     }
 
     private void createTables() {
