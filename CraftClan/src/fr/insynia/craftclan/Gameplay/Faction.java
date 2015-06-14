e6393541a60e56a2dca22fd7776fe8e40afa2d98
Sharowin <Sharowin@gmail.com>
2015-06-14 18:59:00 +0200

    Fix neutralize

--- CraftClan/src/fr/insynia/craftclan/Gameplay/Faction.java
+++ CraftClan/src/fr/insynia/craftclan/Gameplay/Faction.java
@@ -21,6 +21,7 @@ public class Faction implements IDable {
     private int level;
 
     public final static String BASE_FACTION = "Newbie";
+    public final static String BASE_NEUTRAL = "Neutre";
 
     public Faction(int factionId, String name, String color, int level, String status, String leaderName) {
         this.factionId = factionId;
@@ -143,7 +144,7 @@ public class Faction implements IDable {
         List<Point> factionPoints = MapState.getInstance().getFactionPoints(factionId);
         if (factionPoints == null) return false;
         for (Point p : factionPoints) {
-            p.addToFaction(BASE_FACTION);
+            p.addToFaction(BASE_NEUTRAL);
             p.setPointLevel(1);
         }
         return true;
